de72cf42a31e41260d3a805751a89082918db8d6
Nair Sandoval <sandovalnair683@gmail.com>
2026-03-24 12:06:14 -0300

    Primera opción del menú: agregar equipo

--- Actividad_Extra.py
+++ Actividad_Extra.py
@@ -12,3 +12,17 @@ def mostrar_menu():
     print ('5. Salir')
     print ('='*40)
 
+#primera opción: agregar equipo (crear datos de equipo)
+def agregar_equipo(tabla):
+    nombre = input('Nombre del equipo:').strip().title()
+    #validación
+    if nombre in tabla:
+        print(f'El equipo "{nombre}" ya existe')
+        return
+    #crear eqipo con estadísitcas en cero
+    tabla[nombre]= {
+        'puntos':0, 'partidos_jugados':0, 'partidos_ganados':0, 'partidos_empatados':0, 'partidos_perdidos':0, 'goles_a_favor':0,
+        'goles_en_contra':0, 'diferencia_de_goles':0
+    }
+    print(f'Equipo "{nombre}" agregado')
+
