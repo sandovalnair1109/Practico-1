de99c7d669dedb079d98e04599eed44c96487796
Nair Sandoval <sandovalnair683@gmail.com>
2026-03-24 11:52:23 -0300

    Modelado del menú interactivo del torneo de fútbol

---
+++ Actividad_Extra.py
@@ -0,0 +1,14 @@
+#Actividad Extra: Tabla de posiciones de torneo de futbol
+
+#mostrar_menú
+def mostrar_menu():
+    print ('\n'+'='*40)
+    print ('Torneo de fútbol - Tabla de posiciones')
+    print('='*40)
+    print ('1. Agregar equipo')
+    print ('2. Registrar resultado')
+    print ('3. Mostrar tabla')
+    print ('4. Eliminar equipo')
+    print ('5. Salir')
+    print ('='*40)
+
